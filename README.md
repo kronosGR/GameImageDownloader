9dfaaa8a9a6cd91e6dc03f6d009aed93add26df9
kronosGR <geo.elgeo@gmail.com>
2019-07-21 02:08:08 +0200

    ReadME updated UML Screenshot added Empty RecyclerView added Reload RecyclerView added Permission handling added

--- README.md
+++ README.md
@@ -7,10 +7,10 @@ It is an application that connects to an Web site with free games and downloads 
 ## How many activities has the application?
 It has 3 activities:
 1. MainActivity - It is the main activity. Here you can download the RSS Feed and then you can see a list with the titles of the games in this list. You can also, empty the list, reload it again, click an item in the list and start the GameImageActivity. You can also start the SettingsActivity.
-2. GameImageActivity - This activity starts when you click an item in the list from the MainActivity. It shows more information about the game.
+2. GameImageActivity - This activity starts when you click an item in the list from the MainActivity. It shows more information about the game and you can also play a game in your mobile browser.
 3. SettingsActivity - It starts by pressing the settings icon at the options menu at the MainActivity. You can change:
 - The amount of games to be downloaded, maximum is 20.
-- To show the game details at GameImageActivity.
+- To show the description at the recycler view in the MainActivity.
 
 #### Here is the Wire frame
 ![Image Wire frame](https://freegames.kandz.me/wireframe.png)
@@ -18,18 +18,19 @@ It has 3 activities:
 ***
 
 ## How the application works 
-The application has in total 11 classes, includin the 3 activity classes:
-1. MainActivity - It has 9 methods among them loadlist() [Starts the DownloadService] , updateUI() [empties the list and refresh the list with new item], emptyList() [empties the list], OnCreateOptionsMenu()[inflates the options menu] and onOptionsItemsSelected()[handles the clicks on the options menu].
+The application has in total 12 classes, including the 3 activity classes:
+1. MainActivity - It has 9 methods among them loadlist() [Starts the DownloadService] , reload() [empties the list and refresh the list with new item], emptyList() [empties the list], OnCreateOptionsMenu()[inflates the options menu] and onOptionsItemsSelected()[handles the clicks on the options menu].
 2. DownloadService - It is the service started from the MainActivity. The hook method onHandleIntent() starts the download of the RSS feed with the help of FetchRSSTask class
-3. FetchRSSTasl - extends AsyncTask and downloads the RSS Feed. When finishes send a Broadcast to ListReceiver. It also har parseFeed() [parses the feed] , extractImageUrl() and extractDescription() that extract the image and the clean text from the description
-4. ListReceiver - It is a broadcast receiver that receives the action ACTION_VIEW_GET_LISTS. The hook method onReceive() extracts the data from the intent, and calls the updateUI() method from the MainActivity to update the list.
-5. RssListAdapter - is the adapter for the list view at the MainActivity
+3. FetchRSSTasK - extends AsyncTask and downloads the RSS Feed. When finishes updates the MainActivity with the help of WeakReference. It also har parseFeed() [parses the feed] , extractImageUrl() and extractDescription() that extract the image and the clean text from the description
+4. GameReceiver - It is a broadcast receiver that receives the action ACTION_GAME_OPEN. The hook method onReceive() extracts the data from the intent, and starts the activity to show the details for a game
+5. GameRecyclerViewAdapter - is the adapter for the recyclerView at the MainActivity
 6. SettingsActivity - Updates the settings for the Application and stores them to SQLite Database with the help of a Content Provider.
-7. SettinsProvider - is the Content Provides that the SettingsActivity uses.
+7. SettinsProvider - is the Content Provider that the SettingsActivity uses.
 8. SettingsDBHelper - is a class that extends SQLiteOpenHelper which contains a useful set of APIs for managing the database.
-9. SettingsContract - It defines constants that help the application to work with the content URIs, column names and other features of a content provides
-10. Game - is the game object that it helps to store the game information as an object.
+9. SettingsContract - It defines constants that help the application to work with the content URIs, column names and other features of a content provider.
+10. Game - is the game object that it helps to store the game information as an object. It is parcelable.
 11. GameImageActivity - It shows the game information. It has one method, that opens your device's browser so you can visit the website with the online game.
+12. MySingleton - is a singleton class that saves the settings after those have been retrieved from the SQlite Database. It is used so all the activities has access to the settings without the need of using the database all the time.
 
 
 
--- app/src/main/java/me/kandz/gameimagedownloader/MainActivity.java
+++ app/src/main/java/me/kandz/gameimagedownloader/MainActivity.java
@@ -1,16 +1,23 @@
 package me.kandz.gameimagedownloader;
 
+import android.Manifest;
 import android.content.ContentUris;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import me.kandz.gameimagedownloader.Providers.SettingsContract.SettingsEntry;
 import me.kandz.gameimagedownloader.Providers.SettingsDBHelper;
@@ -23,36 +30,38 @@ public class MainActivity extends AppCompatActivity {
 
     SettingsDBHelper mSettingsDBHelper;
     private GameReceiver mReceiver;
+    private RecyclerView mRecyclerView;
+    private static final int INTERNET_PERMISSION_ASKED = 1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
 
     private void readSettings() {
-        Uri rowUri = ContentUris.withAppendedId(SettingsEntry.CONTENT_URI, 1);
+        if (MySingleton.getInstance().getAmountToFetch() <1) {
+            Uri rowUri = ContentUris.withAppendedId(SettingsEntry.CONTENT_URI, 1);
 
-        String[] columns = {
-                SettingsEntry.COL_SHOWDESCRIPTION,
-                SettingsEntry.COL_FETCHAMOUNT
-        };
+            String[] columns = {
+                    SettingsEntry.COL_SHOWDESCRIPTION,
+                    SettingsEntry.COL_FETCHAMOUNT
+            };
 
-        Cursor cursor = getContentResolver().query(rowUri, columns, null, null,
-                null);
+            Cursor cursor = getContentResolver().query(rowUri, columns, null, null,
+                    null);
 
-        int amountToFetchPOS = cursor.getColumnIndex(SettingsEntry.COL_FETCHAMOUNT);
-        int showDescriptionPOS = cursor.getColumnIndex(SettingsEntry.COL_SHOWDESCRIPTION);
+            int amountToFetchPOS = cursor.getColumnIndex(SettingsEntry.COL_FETCHAMOUNT);
+            int showDescriptionPOS = cursor.getColumnIndex(SettingsEntry.COL_SHOWDESCRIPTION);
 
-        cursor.moveToFirst();
+            cursor.moveToFirst();
 
-        //set the settings to the singleton
-        MySingleton.getInstance().setAmountToFetch(cursor.getInt(amountToFetchPOS));
-        MySingleton.getInstance().setShowDescription((cursor.getInt(showDescriptionPOS) == 1) ? true : false);
+            //set the settings to the singleton
+            MySingleton.getInstance().setAmountToFetch(cursor.getInt(amountToFetchPOS));
+            MySingleton.getInstance().setShowDescription((cursor.getInt(showDescriptionPOS) == 1) ? true : false);
+        }
     }
 
     @Override
@@ -62,8 +71,16 @@ public class MainActivity extends AppCompatActivity {
         mSettingsDBHelper = new SettingsDBHelper(this);
         SQLiteDatabase db = mSettingsDBHelper.getReadableDatabase();
 
-        DownloadService.setActivity(this);
-        startService(new Intent(this, DownloadService.class));
+        //check for internet permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) !=
+                PackageManager.PERMISSION_GRANTED){
+            //Ask for internet permission
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.INTERNET},
+                    INTERNET_PERMISSION_ASKED);
+        } else {
+            startDownload();
+        }
 
         //Read the settings values
         readSettings();
@@ -72,6 +89,19 @@ public class MainActivity extends AppCompatActivity {
         mReceiver = new GameReceiver();
         IntentFilter intentFilter = new IntentFilter(GameReceiver.ACTION_GAME);
         registerReceiver(mReceiver, intentFilter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case INTERNET_PERMISSION_ASKED:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    startDownload();
+                else
+                    Toast.makeText(this, "You need internet to download the games", Toast.LENGTH_LONG).show();
+                break;
+
+        }
     }
 
     @Override
@@ -102,16 +132,21 @@ public class MainActivity extends AppCompatActivity {
             case R.id.menu_exit:
                 finish();
                 return true;
+            case R.id.menu_empty:
+                emptyList();
+                return true;
+            case R.id.menu_reload:
+                reload();
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
     /**
-     * It is called from the receiver and updates the UI
-     * @param intent the intent the broadcast receiver will send will the data
+     * Reload the games
      */
-    public void updateUI(Intent intent){
-
+    public void reload(){
+        startDownload();
     }
 
     /**
@@ -125,6 +160,18 @@ public class MainActivity extends AppCompatActivity {
      * empties the list
      */
     private void emptyList(){
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        mRecyclerView.setAdapter(null);
+        Toast.makeText(this,"All games removed", Toast.LENGTH_LONG).show();
+
+    }
+
+    /**
+     * starts the intentservice for downloading
+     */
+    private void startDownload(){
+        DownloadService.setActivity(this);
+        startService(new Intent(this, DownloadService.class));
 
     }
 
